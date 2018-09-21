4c0bbdc405c3ebe2fb2d86791b4e3209ab97667e
Thomas Leese <thomas.leese@digital.cabinet-office.gov.uk>
2018-09-21 08:30:09 +0100

    Simplify `TravelAdviceAlerts`

--- lib/travel_advice_alerts.rb
+++ lib/travel_advice_alerts.rb
@@ -8,41 +8,19 @@ class TravelAdviceAlerts
   EMAIL_DATE_FORMAT = "%l:%M%P, %-d %B %Y".freeze # make sure this matches email-alert-api
 
   def latest_travel_advice_alerts
+    timezone = TZInfo::Timezone.get("Europe/London")
+
     # Extract the countries updated from two days to one hour ago.
     # Unlike with drug alerts, we expect multiple updates from the same set of
     # 225 countries, so search on update time + country rather than the linked url.
     URI.parse(HEALTHCHECK_URL).open do |raw_json|
       JSON.parse(raw_json.read)["editions"]
-        .map { |json_entry| TravelAdviceEntry.new(json_entry) }
-        .map(&:search_value)
-    end
-  end
+        .map do |entry|
+          subject = entry["title"]
+          date = timezone.utc_to_local(Time.parse(entry["published_at"])).strftime(EMAIL_DATE_FORMAT)
 
-  class TravelAdviceEntry
-    attr_reader :entry
-
-    def initialize(entry)
-      @entry = entry
-    end
-
-    def timezone
-      @timezone ||= TZInfo::Timezone.get("Europe/London")
-    end
-
-    def updated_at
-      @updated_at ||= timezone.utc_to_local(Time.parse(entry["published_at"]))
-    end
-
-    def alert_time
-      updated_at.strftime(EMAIL_DATE_FORMAT)
-    end
-
-    def subject
-      entry["title"]
-    end
-
-    def search_value
-      %("#{alert_time}" subject:"#{subject}")
+          %("#{date}" subject:"#{subject}")
+        end
     end
   end
 end
