15db3565dac20e9913c16c577c3d29e449b51027
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2022-11-01 11:40:15 +0000

    Revert "Upgrade Ruby version"

--- lib/task_runner.rb
+++ lib/task_runner.rb
@@ -1,7 +1,7 @@
 require "govuk_app_config"
 
 class TaskRunner
-  def verify_with_retries(verifier:, retries: 5)
+  def verify_with_retries(retries: 5, verifier:)
     tries ||= retries
     verifier.run_report
   rescue StandardError => e
--- spec/features/travel_advice_alert_email_verification_spec.rb
+++ spec/features/travel_advice_alert_email_verification_spec.rb
@@ -17,12 +17,12 @@ RSpec.describe EmailVerifier::TravelAdvice do
   context "when there are travel advice alerts updated between two days and one hour ago" do
     before do
       stub_request(:get, EmailSearch::TravelAdvice::HEALTHCHECK_URL)
-        .to_return(body: File.read("#{File.dirname(__FILE__)}/example_travel_advice_publisher_healthcheck.json"))
+        .to_return(body: File.read(File.dirname(__FILE__) + "/example_travel_advice_publisher_healthcheck.json"))
     end
 
     context "when all emails are sent" do
       before do
-        stub_request(:get, "https://gmail.googleapis.com/gmail/v1/users/me/messages")
+        stub_request(:get, "https://www.googleapis.com/gmail/v1/users/me/messages")
           .with(query: hash_including(:q))
           .to_return(body: { resultSizeEstimate: 1 }.to_json, headers: { "Content-Type" => "application/json" })
       end
@@ -34,7 +34,7 @@ RSpec.describe EmailVerifier::TravelAdvice do
 
       context "when the subject of travel advice doesn't match the country name" do
         before do
-          json = File.read("#{File.dirname(__FILE__)}/example_travel_advice_publisher_healthcheck.json")
+          json = File.read(File.dirname(__FILE__) + "/example_travel_advice_publisher_healthcheck.json")
           json = json.gsub("São Tomé and Principe travel advice", "Sao Tome & Principe travel advice")
           stub_request(:get, EmailSearch::TravelAdvice::HEALTHCHECK_URL).to_return(body: json)
         end
@@ -42,7 +42,7 @@ RSpec.describe EmailVerifier::TravelAdvice do
         it "requests based on the title attribute rather than the country name" do
           expect(verifier.have_all_alerts_been_emailed?).to eql(true)
           expect(
-            a_request(:get, "https://gmail.googleapis.com/gmail/v1/users/me/messages")
+            a_request(:get, "https://www.googleapis.com/gmail/v1/users/me/messages")
             .with(
               query: {
                 q: "\" 3:57pm, 31 March 2016\" subject:\"Sao Tome & Principe travel advice\" from:#{EmailVerifier::FROM_EMAIL} to:#{EmailVerifier::TO_EMAIL}",
@@ -55,11 +55,11 @@ RSpec.describe EmailVerifier::TravelAdvice do
 
       context "when a travel advice item is updated but email has not been sent" do
         before do
-          json = File.read("#{File.dirname(__FILE__)}/example_travel_advice_publisher_healthcheck.json")
+          json = File.read(File.dirname(__FILE__) + "/example_travel_advice_publisher_healthcheck.json")
           json = json.gsub("2016-03-30T12:24:46.000Z", "2016-03-31T15:24:46.000Z")
           stub_request(:get, EmailSearch::TravelAdvice::HEALTHCHECK_URL).to_return(body: json)
 
-          stub_request(:get, "https://gmail.googleapis.com/gmail/v1/users/me/messages")
+          stub_request(:get, "https://www.googleapis.com/gmail/v1/users/me/messages")
             .with(
               query: {
                 q: "\" 4:24pm, 31 March 2016\" subject:\"Albania travel advice\" from:#{EmailVerifier::FROM_EMAIL} to:#{EmailVerifier::TO_EMAIL}",
@@ -78,7 +78,7 @@ RSpec.describe EmailVerifier::TravelAdvice do
 
     context "when no emails are sent" do
       before do
-        stub_request(:get, "https://gmail.googleapis.com/gmail/v1/users/me/messages")
+        stub_request(:get, "https://www.googleapis.com/gmail/v1/users/me/messages")
           .with(query: hash_including(:q))
           .to_return(body: { resultSizeEstimate: 0 }.to_json, headers: { "Content-Type" => "application/json" })
       end
