c98b4208357446dd3373948c8887a3a586f98cfc
Peter Hartshorn <peter.hartshorn@digital.cabinet-office.gov.uk>
2022-11-02 10:46:16 +0000

    Fix styling

--- lib/task_runner.rb
+++ lib/task_runner.rb
@@ -1,7 +1,7 @@
 require "govuk_app_config"
 
 class TaskRunner
-  def verify_with_retries(retries: 5, verifier:)
+  def verify_with_retries(verifier:, retries: 5)
     tries ||= retries
     verifier.run_report
   rescue StandardError => e
